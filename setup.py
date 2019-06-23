4d7872805a8da29350115258d8b6f12a91958fb2
Tomasz <tomasz.j.gawel@gmail.com>
2019-06-23 10:49:06 +0200

    Changed setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='chat_protocol',
-    version='2.0.1',
-    packages=['namedtuple,unittest,datetime,time'],
+    version='2.0.2',
+    packages=[''],
     url='https://github.com/tomaszgawel/chat-protocol',
     license='gpl-3.0',
     author='tomaszgawel, kpakula, akulinski',
